e8cc95be9ffd908f4eff861511df8c8a0ae9170c
DrPsychick <drpsychick@drsick.de>
2019-10-01 14:43:40 +0200

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir $HOME/.ssh
-chmod 755 $HOME/.ssh
+chmod 700 $HOME/.ssh
 
-echo "Copy your SSH key to $HOME/.ssh and add it to `authorized_keys` file (cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys)"
+echo "Copy your SSH key to '$HOME/.ssh' and add it to your 'authorized_keys' file (cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys)"
