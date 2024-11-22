b953ba5bd24e7035d865f82bd6f213bc86a2c627
DrPsychick <drpsychick@drsick.de>
2024-11-22 22:16:43 +0100

    feat: :sparkles: fetch files/templates from storage before running (#9)
    Support 1password, vault and web

---
+++ templates/git-repos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# a simple script to print which user name/email is used for your git repos (i.e verify gitconfig).
+
+BASEDIR=${1:="."}
+
+for d in $(find $BASEDIR -type d -a -name .git); do
+  dir=$(dirname $d)
+  (cd $dir
+  name=$(git config --get user.name)
+  email=$(git config --get user.email)
+  if [ -z "$name" -o -z "$email" ]; then
+    echo "MISSING gitconfig: $dir is not configured $(git config --get remote.origin.url)"
+  else
+    echo "$name / $email : $dir"
+  fi
+  )
+done
